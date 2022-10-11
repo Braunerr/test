d507e4fb05877f822ce5f318ef593dbc137622b7
Braüner <mathn519@gmail.com>
2022-10-11 11:04:52 +0200

    Add files via upload

---
+++ DataService.cs
@@ -0,0 +1,83 @@
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+
+using Model;
+
+namespace Service;
+
+public class DataService
+{
+    private PostContext db { get; }
+
+    public DataService(PostContext db)
+    {
+        this.db = db;
+    }
+
+    /// <summary>
+    /// Seeder noget nyt data i databasen hvis det er nødvendigt.
+    /// </summary>
+    public void SeedData()
+    {
+        User user = db.Users.FirstOrDefault()!;
+        if (user == null)
+        {
+            user = new User { Username = "Kristian" };
+            db.Users.Add(user);
+            db.Users.Add(new User { Username = "Søren" });
+            db.Users.Add(new User { Username = "Mette" });
+            db.SaveChanges();
+        }
+
+        Post post = db.Posts.FirstOrDefault()!;
+        if (post == null)
+        {
+            post = new Post { PostName = "Harry Potter", User = user };
+            db.Posts.Add(post);
+            db.Posts.Add(new Post { PostName = "Ringenes Herre", User = user });
+            db.Posts.Add(new Post { PostName = "Entity Framework for Dummies", User = user });
+            db.SaveChanges();
+        }
+
+        Comment comment = db.Comments.FirstOrDefault()!;
+        if (comment == null)
+        {
+            post.Comments.Add(new Comment { Text = "bedst komentar", User = user });
+            post.Comments.Add(new Comment { Text = "Værste komentar", User = user });
+            db.SaveChanges();
+        }
+    }
+
+    public List<Post> GetPosts()
+    {
+        return db.Posts.Include(b => b.User).ToList();
+    }
+
+    public List<User> GetUsers()
+    {
+        return db.Users.ToList();
+    }
+
+    public User GetUser(int id)
+    {
+        return db.Users.FirstOrDefault(a => a.UserId == id);
+    }
+
+    public Post GetPost(int id)
+    {
+        return db.Posts
+            .Include(p => p.User)
+            .Include(b => b.Comments)
+            .FirstOrDefault(a => a.PostId == id); //join User på en Post
+    }
+
+    //public Comment GetComment();
+
+    public string CreatePost(string PostName, int UserId)
+    {
+        User user = db.Users.FirstOrDefault(a => a.UserId == UserId);
+        db.Posts.Add(new Post { PostName = PostName, User = user });
+        db.SaveChanges();
+        return "Post created";
+    }
+}
